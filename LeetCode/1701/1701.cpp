1c9df47657d2d2a73cdbcc5fbf5389ac04766a80
Ander Liu <anderliu0216@gmail.com>
2024-07-21 18:36:28 -0500

    Update 1701.cpp

--- LeetCode/1701/1701.cpp
+++ LeetCode/1701/1701.cpp
@@ -1,1 +1,1 @@
-struct Solution{double averageWaitingTime(vector<vector<int>>c){double s=0;int t=0;for(auto i:c)t=max(t,i[0])+i[1],s+=t-i[0];return s/c.size();}};
+struct Solution{double averageWaitingTime(vector<vector<int>>c){double s;int t=0;for(auto i:c)t=max(t,i[0])+i[1],s+=t-i[0];return s/c.size();}};
