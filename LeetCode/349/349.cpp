857f6e83e203438747886b4c74ea9e1ff30b55e9
Ander Liu <anderliu0216@gmail.com>
2024-07-21 17:48:59 -0500

    Update 349.cpp

--- LeetCode/349/349.cpp
+++ LeetCode/349/349.cpp
@@ -1,1 +1,1 @@
-using V=vector<int>;struct Solution{V intersect(V n,V m){V r,u(2e3);for(auto i:n)u[i]++;for(auto i:m)if(u[i]-->0)r.push_back(i);return r;}};
+using V=vector<int>;struct Solution{V intersection(V n,V m){V r,u(2e3);for(auto i:n)u[i]=1;for(auto i:m)if(u[i]-->0)r.push_back(i);return r;}};
